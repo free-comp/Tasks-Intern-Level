5d6529e3421f4fb8b9cf8601c42b605529b7c019
Alisa_Brk <free-comp@yandex.ru>
2021-11-15 11:54:24 +0300

    Added the solution to the first task

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1. Вывести квдрат числа
+
+double Square (double x)
+{
+    double result =  x*x;
+    return result;
+}
+
+Console.WriteLine("Введите число");
+string Number = Console.ReadLine();
+//int numb = Convert.ToInt32(Number);
+
+double numb = Square(Convert.ToDouble(Number)); 
+
+Console.WriteLine($"Квадрат введенного числа равен: {numb}");
