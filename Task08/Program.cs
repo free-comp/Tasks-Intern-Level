5ee114108392585563aecb65986436687730e701
Alisa_Brk <free-comp@yandex.ru>
2021-11-16 21:22:39 +0300

    Added solution to the Task08 (output numbers from -N to N)

---
+++ Task08/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 8. Показать числа от -N до N
+
+Console.WriteLine("Задача 8. Показать числа от -N до N");
+
+// Console.Write("Введите начало диапазона: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите окончание диапазона: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b=-1*a;;
+
+if (b < a)
+{
+    int c = b; 
+    b = a; a = c;
+}
+
+Console.WriteLine($"Будем рассматривать отрезок [{a},{b}]");
+Console.WriteLine($"Числовая последовательность на отрезке [{a},{b}]: ");
+int i = a;
+while(i<=b)
+{
+    Console.Write($" {i}");
+    i++;
+}
+Console.WriteLine();
