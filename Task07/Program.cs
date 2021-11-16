e19f0ca656295827896ab9f44bb470ce2f25e318
Alisa_Brk <free-comp@yandex.ru>
2021-11-16 16:56:27 +0300

    Added solution to the Task07 (cheking for even numbers)

---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 7. Выяснить является ли число чётным
+
+Console.Write("Введите целое число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+
+if (numb % 2 == 0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+else
+{
+    Console.WriteLine("Введенное число нечетное");
+}
