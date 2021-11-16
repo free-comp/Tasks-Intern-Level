64de5c2de5d1fe0b6ee433b4ca56d71c4d4c7aa3
Alisa_Brk <free-comp@yandex.ru>
2021-11-16 23:24:01 +0300

    Added solution to the task10 (output of the last digit of a three-digit numbe)

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10. Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Задача 10. Показать последнюю цифру трёхзначного числа.");
+Console.WriteLine();
+
+Console.Write("Введите трёхзначное число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb < 0)
+{
+    numb = -1 * numb;
+}
+
+if (numb < 100 || numb > 999)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число. Попробуйте еще раз.");
+    Environment.Exit(0);
+}
+int ost = numb % 10;
+Console.WriteLine($"Последняя цифра введенного числа равна: {ost}");
