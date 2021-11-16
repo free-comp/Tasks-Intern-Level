ab6ff41a363d2b1cd89ce4d3bc0d93eb95eeb53c
Alisa_Brk <free-comp@yandex.ru>
2021-11-16 13:50:45 +0300

    Added solution to the fourth Task (about daysof week)

---
+++ Task04/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4. По заданному номеру дня недели вывести его название
+
+
+Console.Write("Введите число от 1 до 7: ");
+int Numb = Convert.ToInt16(Console.ReadLine());
+switch (Numb)
+{
+    case 1:
+        Console.WriteLine("Первый день недели понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Второй день недели вторник");
+        break;
+    case 3:
+        Console.WriteLine("Третий день недели седа");
+        break;
+    case 4:
+        Console.WriteLine("Четвертый день недели четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятый день недели пятница");
+        break;
+    case 6:
+        Console.WriteLine("Шестой день недели суббота");
+        break;
+    case 7:
+        Console.WriteLine("Седьмой день недели воскресенье");
+        break;
+    default:
+        Console.WriteLine("Вы ввели число, не соответствующее диапазону от 1 до 7");
+        break;
+}
