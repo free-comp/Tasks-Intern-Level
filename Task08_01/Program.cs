eb6a968ff56532fd6928c60b40d8aaea6737dbbe
Alisa_Brk <free-comp@yandex.ru>
2021-11-16 21:24:11 +0300

    Added solution to the Task08_01 (output numbers from -n to M)

---
+++ Task08_01/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 8. Показать числа от -N до M
+
+Console.WriteLine("Задача 8. Показать числа от -N до N");
+
+Console.Write("Введите начало диапазона: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите окончание диапазона: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b < a)
+{
+    int c = b; 
+    b = a; a = c;
+}
+
+Console.WriteLine($"Вы задали отрезок [{a},{b}]");
+Console.WriteLine($"Числовая последовательность на отрезке [{a},{b}]: ");
+int i = a;
+while(i<=b)
+{
+    Console.Write($" {i}");
+    i++;
+}
+Console.WriteLine();
