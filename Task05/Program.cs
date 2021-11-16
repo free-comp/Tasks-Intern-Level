6e9212a5bf5b4d885cf4a3813c59f1c4e25d2593
Alisa_Brk <free-comp@yandex.ru>
2021-11-16 16:19:55 +0300

    Added solution to the Task05 (finding the max number)

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти максимальное из трех чисел
+int Max(int x1, int x2, int x3)
+{
+    int result = x1;
+    if (x2 > result) result = x2;
+    if (x3 > result) result = x3;
+    return result;
+}
+
+int a = new Random().Next(-1000, 1000);
+int b = new Random().Next(-1000, 1000);
+int c = new Random().Next(-1000, 1000);
+Console.WriteLine($" Даны числа: {a}, {b}, {c}");
+
+int max = Max(a, b, c);
+
+Console.WriteLine($"Максимальное из трех чисел: {max}");
