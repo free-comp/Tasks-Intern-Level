d49828f979b72e7a26603484accb27b7b1ec362f
Alisa_Brk <free-comp@yandex.ru>
2021-11-16 23:40:42 +0300

    Added solution to the Task11 (Output of the second digit of a three-digit number)

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 11. Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Задача 11. Показать вторую цифру трёхзначного числа.");
+Console.WriteLine();
+
+Console.Write("Введите трёхзначное число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb < 0)
+{
+    numb = -1 * numb;
+}
+
+if (numb < 100 || numb > 999)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число. Попробуйте еще раз.");
+    Environment.Exit(0);
+}
+
+int SecondDigit = (numb % 100 - numb % 10) / 10;
+Console.WriteLine($"Вторая цифра введенного числа равна: {SecondDigit}");
