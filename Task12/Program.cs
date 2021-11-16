a798fa091d81fc597e8b1e3c2a9ec8f1c5950c2d
Alisa_Brk <free-comp@yandex.ru>
2021-11-17 00:04:41 +0300

    Added solution to the Task12 (the maximum digit of a two-digit number)

---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 12. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.WriteLine("Задача 12. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.");
+
+int numb = new Random().Next(10, 99);
+Console.WriteLine($"Дано число: {numb}");
+
+int FirstDigit = numb % 10;
+int SecondDigit = (numb - FirstDigit) / 10;
+
+int max = FirstDigit;
+if (SecondDigit > max) max = SecondDigit;
+
+Console.WriteLine($"Наибольшая цифра в числе равна: {max}");
