37167afb8c4db4acba8d13a4deab6777d05093a6
Alisa_Brk <free-comp@yandex.ru>
2021-11-18 11:12:51 +0300

    Solution of the task14 (checking for multiplicity)

---
+++ Task14/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 14. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Задача 14. Выяснить, кратно ли число заданному, если нет, вывести остаток.");
+
+int OriginalNumb = new Random().Next(-1000, 1000);
+int CheckNumb = new Random().Next(1, 100);
+
+Console.WriteLine($"Проверим число {OriginalNumb} на кратность числу: {CheckNumb}");
+
+int result = OriginalNumb % CheckNumb;
+if (result < 0) result = -1 * result;
+
+if (result == 0)
+{
+    Console.WriteLine($"Число {CheckNumb} кратно {OriginalNumb}");
+}
+else
+{
+    Console.WriteLine($"Число {CheckNumb} не кратно {OriginalNumb}. Остаток деления равен: {result}");
+}
+
+
