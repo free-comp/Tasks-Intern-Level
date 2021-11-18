eecc0d8bf3f8ce6be0d884ef8f3e402f43c23593
Alisa_Brk <free-comp@yandex.ru>
2021-11-18 10:43:37 +0300

    Solution of the third task (removimg the second digit)

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13. Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Задача 13. Удалить вторую цифру трёхзначного числа.");
+
+int numb = new Random().Next(100, 999);
+Console.WriteLine($"Дано число: {numb}");
+
+//Вариант решения через мат.операции
+int FirstDigit = numb % 10;
+int SecondDigit = (numb % 100 - numb % 10) / 10;
+int ThirdDigit = (numb - numb % 100) / 100;
+
+int rezult = ThirdDigit * 10 + FirstDigit;
+
+Console.WriteLine($"Если удалить вторую цифру из данного числа, получим новое число, равное: {rezult}");
+
+// Вариант решения через строки и символы
+// string Numb = Convert.ToString(numb);
+// char [] mas = Numb.ToCharArray();
+// char n1 = mas[0];
+// char n2 = mas[2];
+
+// string N = string.Concat(n1,n2);
+// int rezult = Convert.ToInt32(N);
+// Console.WriteLine($"Если удалить вторую цифру из данного числа, получим новое число, равное: {rezult}");
