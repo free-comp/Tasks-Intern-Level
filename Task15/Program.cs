8bc084d497b7d7a3564a5ba3fee7154cb206969c
Alisa_Brk <free-comp@yandex.ru>
2021-11-18 12:26:31 +0300

    Solution of the Task15 (finding third digit)

---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15. Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Задача 15. Найти третью цифру числа или сообщить, что её нет");
+
+int numb = new Random().Next(-300, 300);
+Console.WriteLine($"Дано число: {numb}");
+if (numb < 0) numb = -1 * numb;
+
+numb = numb % 1000;
+if (numb < 100)
+{
+    Console.WriteLine("В числе нет третьей цифры");
+}
+else
+{
+    int ThirdDigit = (numb - numb % 100) / 100;
+    Console.WriteLine($"Третья цифра числа равна {ThirdDigit}");
+}
+
+
